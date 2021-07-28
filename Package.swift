4efa82421142d1b59a79992565938ce3e890eb0e
GothStar <vla9659@gmail.com>
2021-07-28 23:48:10 +0300

    Small fixes in package manager

--- Package.swift
+++ Package.swift
@@ -4,20 +4,21 @@
 import PackageDescription
 
 let package = Package(
-  name: "SwiftSDL2",
-  products: [
-    .library(name: "SwiftSDL2", targets: ["SwiftSDL2"])
-  ],
-  targets: [
-    .systemLibrary(name: "CSDL2", pkgConfig: "sdl2"),
-    .systemLibrary(name: "CSDL2_image",
-                   pkgConfig: "sdl2_image",
-                   providers: [
-                    .brew(["sdl2_image"])
-                   ]),
-    .target(name: "SwiftSDL2", dependencies: ["CSDL2", "CSDL2_image"]),
-    .executableTarget(name: "SwiftSDL2Demo", dependencies: ["SwiftSDL2"]),
-    .executableTarget(name: "SwiftSDL2ThreadDemo", dependencies: ["SwiftSDL2"]),
-    .testTarget(name: "SwiftSDL2Tests", dependencies: ["SwiftSDL2"])
-  ]
+    name: "SwiftSDL2",
+    products: [
+        .library(name: "SwiftSDL2", targets: ["SwiftSDL2"])
+    ],
+    targets: [
+        .systemLibrary(name: "CSDL2", pkgConfig: "sdl2"),
+        .systemLibrary(name: "CSDL2_image",
+                       path: "Sources/CSDL2_image",
+                       pkgConfig: "sdl2_image",
+                       providers: [
+                        .brew(["sdl2_image"])
+                       ]),
+        .target(name: "SwiftSDL2", dependencies: ["CSDL2", "CSDL2_image"]),
+        .executableTarget(name: "SwiftSDL2Demo", dependencies: ["SwiftSDL2"]),
+        .executableTarget(name: "SwiftSDL2ThreadDemo", dependencies: ["SwiftSDL2"]),
+        .testTarget(name: "SwiftSDL2Tests", dependencies: ["SwiftSDL2"])
+    ]
 )
